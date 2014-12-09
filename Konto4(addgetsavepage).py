02685c515ba935fd3f652e259e6fa57da6acf143
tan3warut4 <itanprscritical@gmail.com>
2014-12-10 01:21:45 +0700

    Konto4
    Add gestate page

---
+++ Konto4(addgetsavepage).py
@@ -0,0 +1,163 @@
+Python 2.7.9rc1 (v2.7.9rc1:40eada278702, Nov 25 2014, 17:25:50) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>> ================================ RESTART ================================
+>>> 
+
+Traceback (most recent call last):
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 59, in <module>
+    Main()
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 11, in __init__
+    Button(self.main, text='New User', command = self.newname).place(x=140,y=200)
+AttributeError: Main instance has no attribute 'newname'
+>>> ================================ RESTART ================================
+>>> 
+
+>>> ================================ RESTART ================================
+>>> 
+
+Traceback (most recent call last):
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 59, in <module>
+    Main()
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 11, in __init__
+    Button(self.main, text='New User', command = self.newname).place(x=140,y=200)
+AttributeError: Main instance has no attribute 'newname'
+>>> ================================ RESTART ================================
+>>> 
+
+Traceback (most recent call last):
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 59, in <module>
+    Main()
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 11, in __init__
+    Button(self.main, text='New User', command = self.Newname).place(x=140,y=200)
+AttributeError: Main instance has no attribute 'Newname'
+>>> ================================ RESTART ================================
+>>> 
+
+Traceback (most recent call last):
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 59, in <module>
+    Main()
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 11, in __init__
+    Button(self.main, text='New User', command = self.New).place(x=140,y=200)
+AttributeError: Main instance has no attribute 'New'
+>>> ================================ RESTART ================================
+>>> 
+
+Traceback (most recent call last):
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 59, in <module>
+    Main()
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 11, in __init__
+    Button(self.main, text='New User', command = self.New).place(x=140,y=200)
+AttributeError: Main instance has no attribute 'New'
+>>> ================================ RESTART ================================
+>>> 
+
+Traceback (most recent call last):
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 45, in <module>
+    Main()
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 11, in __init__
+    Button(self.main, text='New User', command = self.New).place(x=140,y=200)
+AttributeError: Main instance has no attribute 'New'
+>>> ================================ RESTART ================================
+>>> 
+
+>>> ================================ RESTART ================================
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk/Tkinter.py", line 1532, in __call__
+    return self.func(*args)
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 35, in getslary
+    self.new.destroy()
+AttributeError: New instance has no attribute 'new'
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk/Tkinter.py", line 1532, in __call__
+    return self.func(*args)
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 35, in getslary
+    self.new.destroy()
+AttributeError: New instance has no attribute 'new'
+
+>>> ================================ RESTART ================================
+>>> 
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk/Tkinter.py", line 1532, in __call__
+    return self.func(*args)
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 35, in getslary
+    self.new.destroy()
+AttributeError: New instance has no attribute 'new'
+
+>>> ================================ RESTART ================================
+>>> 
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk/Tkinter.py", line 1532, in __call__
+    return self.func(*args)
+  File "/Users/Tan/Desktop/KONTO Project/Konto2(fix newuser layout).py", line 35, in getslary
+    self.New.destroy()
+AttributeError: New instance has no attribute 'New'
+
+>>> ================================ RESTART ================================
+>>> 
+>>> ================================ RESTART ================================
+>>> 
+
+>>> ================================ RESTART ================================
+>>> 
+
+>>> ================================ RESTART ================================
+>>> 
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk/Tkinter.py", line 1532, in __call__
+    return self.func(*args)
+  File "/Users/Tan/Desktop/KONTO Project/Konto3(add slary page).py", line 36, in getslary
+    Getslary()
+  File "/Users/Tan/Desktop/KONTO Project/Konto3(add slary page).py", line 47, in __init__
+    Button(self.main, text='Go next', command = self.getsave).place(x=50, y = 250)
+AttributeError: Getslary instance has no attribute 'getsave'
+
+>>> ================================ RESTART ================================
+>>> 
+Exception in Tkinter callback
+Traceback (most recent call last):
+  File "/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk/Tkinter.py", line 1532, in __call__
+    return self.func(*args)
+  File "/Users/Tan/Desktop/KONTO Project/Konto3(add slary page).py", line 36, in getslary
+    Getslary()
+  File "/Users/Tan/Desktop/KONTO Project/Konto3(add slary page).py", line 47, in __init__
+    Button(self.main, text='Go next', command = self.getsave).place(x=50, y = 250)
+AttributeError: Getslary instance has no attribute 'getsave'
+
+>>> ================================ RESTART ================================
+>>> 
+
+>>> ================================ RESTART ================================
+>>> 
