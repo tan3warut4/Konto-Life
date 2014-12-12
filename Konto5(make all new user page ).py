ea8811f7f374504fb49e19224024bc662947683e
Wirun leelachai <s7070107@kmitl.ac.th>
2014-12-12 22:32:34 +0700

    update some of database
    test

--- Konto5(make all new user page ).py
+++ Konto5(make all new user page ).py
@@ -1,4 +1,6 @@
 from Tkinter import *
+import sqlite3
+
 class Main():
      def __init__(self):
           self.main = Tk()
@@ -14,10 +16,92 @@ class Main():
           self.main.mainloop()
      def new(self):
           self.main.destroy()
-          New()
+          self.main = Tk()
+          self.main.geometry("500x400+500+100")
+          self.main.title("New")
+          self.main.resizable(width=FALSE, height=FALSE)
+          Label(self.main, text = 'Welcome', font=("Helvetica", 30)).place(x=160, y=30)
+          Label(self.main, text = 'please enter your name', font=("Helvetica", 25)).place(x=80, y=80)
+          self.x = Entry(self.main)
+          self.x.place(x=180, y=150)
+          Button(self.main, text='Go', command = self.getslary, font=("Helvetica", 18)).place(x=215, y = 200)
+
+     def getslary(self):
+          self.name = self.x.get()
+          self.main.destroy()
+          self.slary()
+
+     def slary(self):
+          self.main = Tk()
+          self.main.geometry("500x400+500+100")
+          self.main.title("New")
+          self.main.resizable(width=FALSE, height=FALSE)
+          Label(self.main, text = 'Hello!', font=("Helvetica", 30)).place(x=198, y=30)
+          Label(self.main, text = 'Let me know your salary', font=("Helvetica", 25)).place(x=80, y=90)
+          self.x = Entry(self.main)
+          self.x.place(x=180, y=150)
+          Button(self.main, text='Go', command = self.getsave, font=("Helvetica", 18)).place(x=215, y = 200)
+     def getsave(self):
+          self.slary = self.x.get()
+          self.main.destroy()
+          self.save()
+     def save(self):
+          self.main = Tk()
+          self.main.geometry("500x400+500+100")
+          self.main.title("New")
+          self.main.resizable(width=FALSE, height=FALSE)
+          Label(self.main, text = 'In the end of the month ', font=("Helvetica", 30)).place(x=60, y=30)
+          Label(self.main, text = 'You want to safe', font=("Helvetica", 30)).place(x=110, y=90)
+          self.x = Entry(self.main)
+          self.x.place(x=180, y=150)
+          Button(self.main, text='Go next', command = self.proceed, font=("Helvetica", 18)).place(x=190, y = 200)
+
+     def proceed(self):
+          self.value = self.x.get()
+          self.main.destroy()
+          self.proceed2()
+     def proceed2(self):
+          self.main = Tk()
+          self.main.geometry("500x400+500+100")
+          self.main.title("New")
+          self.main.resizable(width=FALSE, height=FALSE)
+          Label(self.main, text = 'Let me Proceed ', font=("Helvetica", 40)).place(x=60, y=50)
+          self.x = Entry(self.main)
+          self.x.place(x=180 , y=150)
+          Button(self.main, text='Go next', command = self.resultuse, font=("Helvetica", 18)).place(x=190, y = 200)
+     def resultuse(self):
+          self.value = self.x.get()
+          self.main.destroy()
+          self.run_resultuse()
+     def run_resultuse(self):
+          self.main = Tk()
+          self.main.geometry("500x400+500+100")
+          self.main.title("New")
+          self.main.resizable(width=FALSE, height=FALSE)
+          Label(self.main, text = 'You need to Use', font=("Helvetica", 40)).place(x=50, y=50)
+          self.x = Entry(self.main)
+          self.x.place(x=180, y=150)
+          Button(self.main, text='Let start save monney', command = self.getold, font=("Helvetica", 15)).place(x=130, y = 200)
+     def getold(self):
+          self.value = self.x.get()
+          self.main.destroy()
+          self.run_old()
+          
+     def run_old(self):
+          c.execute("INSERT INTO stocks VALUES (1, 'test', 200, 200, 1)")
+          self.main = Tk()
+          self.main.geometry("500x400+500+100")
+          self.main.title("Old")
+          self.main.resizable(width=FALSE, height=FALSE)
+          Label(self.main, text = 'Name ' + self.name , font=("Helvetica", 40)).place(x=50, y=30)
+          Label(self.main, text = 'Day', font=("Helvetica", 40)).place(x=50, y=90)
+          Label(self.main, text = 'Draft', font=("Helvetica", 40)).place(x=50, y=140)
+          Label(self.main, text = 'You use', font=("Helvetica", 40)).place(x=50, y=200)
+
      def old(self):
           self.main.destroy()
           Old()
+     
 
 class New():
      def __init__(self):
@@ -27,12 +111,13 @@ class New():
           self.main.resizable(width=FALSE, height=FALSE)
           Label(self.main, text = 'Welcome', font=("Helvetica", 30)).place(x=160, y=30)
           Label(self.main, text = 'please enter your name', font=("Helvetica", 25)).place(x=80, y=80)
-          self.x = Entry(self.main).place(x=180, y=150)
+          self.x = Entry(self.main)
+          self.x.place(x=180, y=150)
           Button(self.main, text='Go', command = self.getslary, font=("Helvetica", 18)).place(x=215, y = 200)
 
      def getslary(self):
           self.main.destroy()
-          Getslary()
+          #Getslary()
 class Getslary():
      def __init__(self):
           self.main = Tk()
@@ -41,11 +126,13 @@ class Getslary():
           self.main.resizable(width=FALSE, height=FALSE)
           Label(self.main, text = 'Hello!', font=("Helvetica", 30)).place(x=198, y=30)
           Label(self.main, text = 'Let me know your salary', font=("Helvetica", 25)).place(x=80, y=90)
-          self.x = Entry(self.main).place(x=180, y=150)
+          self.x = Entry(self.main)
+          self.x.place(x=180, y=150)
           Button(self.main, text='Go', command = self.getsave, font=("Helvetica", 18)).place(x=215, y = 200)
      def getsave(self):
+          slary = self.x.get()
           self.main.destroy()
-          Getsave()
+          #Getsave()
 
 class Getsave():
      def __init__(self):
@@ -55,12 +142,14 @@ class Getsave():
           self.main.resizable(width=FALSE, height=FALSE)
           Label(self.main, text = 'In the end of the month ', font=("Helvetica", 30)).place(x=60, y=30)
           Label(self.main, text = 'You want to safe', font=("Helvetica", 30)).place(x=110, y=90)
-          self.x = Entry(self.main).place(x=180, y=150)
+          self.x = Entry(self.main)
+          self.x.place(x=180, y=150)
           Button(self.main, text='Go next', command = self.proceed, font=("Helvetica", 18)).place(x=190, y = 200)
 
      def proceed(self):
+          self.value = self.x.get()
           self.main.destroy()
-          Proceed()
+          #Proceed()
 class Proceed():
      def __init__(self):
           self.main = Tk()
@@ -68,11 +157,13 @@ class Proceed():
           self.main.title("New")
           self.main.resizable(width=FALSE, height=FALSE)
           Label(self.main, text = 'Let me Proceed ', font=("Helvetica", 40)).place(x=60, y=50)
-          self.x = Entry(self.main).place(x=180, y=150)
+          self.x = Entry(self.main)
+          self.x.place(x=180, y=150)
           Button(self.main, text='Go next', command = self.resultuse, font=("Helvetica", 18)).place(x=190, y = 200)
      def resultuse(self):
+          self.value = self.x.get()
           self.main.destroy()
-          Resultuse()
+          #Resultuse()
 class Resultuse():
      def __init__(self):
           self.main = Tk()
@@ -80,23 +171,35 @@ class Resultuse():
           self.main.title("New")
           self.main.resizable(width=FALSE, height=FALSE)
           Label(self.main, text = 'You need to Use', font=("Helvetica", 40)).place(x=50, y=50)
-          self.x = Entry(self.main).place(x=180, y=150)
+          self.x = Entry(self.main)
+          self.x.place(x=180, y=150)
           Button(self.main, text='Let start save monney', command = self.old, font=("Helvetica", 15)).place(x=130, y = 200)
      def old(self):
+          self.value = self.x.get()
           self.main.destroy()
-          Old()
+          #Old()
 
 class Old():
      def __init__(self):
+          print name
           self.main = Tk()
           self.main.geometry("500x400+500+100")
           self.main.title("Old")
           self.main.resizable(width=FALSE, height=FALSE)
-          Label(self.main, text = 'Name', font=("Helvetica", 40)).place(x=50, y=30)
+          Label(self.main, text = 'Name' + name , font=("Helvetica", 40)).place(x=50, y=30)
           Label(self.main, text = 'Day', font=("Helvetica", 40)).place(x=50, y=90)
           Label(self.main, text = 'Draft', font=("Helvetica", 40)).place(x=50, y=140)
           Label(self.main, text = 'You use', font=("Helvetica", 40)).place(x=50, y=200)
 
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
 
-
+def _init():
+     #c.execute('''CREATE TABLE stocks
+     #        (id, name, slary, save, month)''')
+     c.execute("INSERT INTO stocks VALUES (1, 'test', 200, 200, 1)")
+     conn.commit()
+_init()
+for row in c.execute('SELECT * FROM stocks'):
+        print row
 Main()
